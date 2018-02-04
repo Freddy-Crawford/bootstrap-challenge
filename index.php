27fa8ea482cf44210fc5d542c107494ca271147c
freddy-crawford <freddy_crawford@yahoo.com>
2018-02-03 20:17:17 -0700

    started bootstrap challenge , added main structure for challenge

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	 <title>bootstrap challenge</title>
+
+  <meta charset="UTF-8">
+
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+
+
+	<nav class="navbar navbar-default">
+
+		<div class="container-fluid">
+
+			<div class="navbar-header">
+
+				<a href="#" class="navbar-brand">MyNavBar</a>
+
+			</div>
+
+		</div>
+
+  	</nav>
+
+
+
+  			<div class="jumbotron">
+
+  				<h1>HELLO WORLD!</h1>
+
+  				<p>this is home hafe gare hhild smark thass trdsdd hskaj htr fsccd kleri</p>
+
+  				<button class="btn btn primary" role="button">Learn More</button>
+
+			</div>
+
+
+
+		<div class="container">
+
+			<p>Lorizzle ipsum dolizzle gangsta amet, shiznit adipiscing tellivizzle. Nullizzle yippiyo velizzle, things crackalackin, dang the bizzle, gravida vizzle, arcu. Pellentesque stuff tortizzle. Sizzle eros. Fusce izzle shizzle my nizzle crocodizzle dapibizzle turpis tempizzle dawg. Mauris mah nizzle nibh et cool. Vestibulum izzle tortizzle. Pellentesque crazy rhoncizzle ma nizzle. In you son of a bizzle habitasse platea dictumst. Donec check out this. Curabitur tellus its fo rizzle, pretium eu, mattizzle izzle, eleifend vitae, yo. Daahng dawg fizzle. Yo semper velizzle sizzle purizzle.</p>
+
+			<img src="bootstrap-challenge.html" width="200" height="100">
+
+		</div>
+
+
+
+
+
+
+		<div class="container">
+
+			<div class="row">
+
+					<div class="card border-0 col-lg-3 col-md-6 col-sm-6 col-xs-12">
+
+				<div class="card-block border rounded m-2 p-2">
+
+					<p class="card-text">Gangster interdum volutpat tellizzle. Ut for sure adipiscing lorem. Donec</p>
+
+				</div>
+
+					</div>
+
+			 <div class="card border-0 col-lg-3 col-md-6 col-sm-6 col-xs-12">
+
+          				<div class="card-block border rounded m-2 p-2">
+
+          					<p class="card-text">Gangster interdum volutpat tellizzle. Ut for sure adipiscing lorem. Donec</p>
+
+          				</div>
+
+          					</div>
+
+          		<div class="card border-0 col-lg-3 col-md-6 col-sm-6 col-xs-12">
+
+               				<div class="card-block border rounded m-2 p-2">
+
+               					<p class="card-text">Gangster interdum volutpat tellizzle. Ut for sure adipiscing lorem. Donec</p>
+
+               				</div>
+
+               					</div>
+
+               <div class="card border-0 col-lg-3 col-md-6 col-sm-6 col-xs-12">
+
+               				<div class="card-block border rounded m-2 p-2">
+
+               					<p class="card-text">Gangster interdum volutpat tellizzle. Ut for sure adipiscing lorem. Donec</p>
+
+               				</div>
+
+               					</div>
+
+
+
+
+</div>
+</div>
+
+</body>
+</html>
