08e55482cd3fa07e028a4660d57439d2a2081f93
freddy-crawford <freddy_crawford@yahoo.com>
2018-02-04 20:13:23 -0700

    made an attempt at the challenge ,

--- index.php
+++ index.php
@@ -4,30 +4,37 @@
 	 <title>bootstrap challenge</title>
 
   <meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 </head>
 
 <body>
 
 
-	<nav class="navbar navbar-default">
+	<nav class="navbar navbar-navbar-light bg-light">
 
 		<div class="container-fluid">
 
 			<div class="navbar-header">
 
-				<a href="#" class="navbar-brand">MyNavBar</a>
+				<a href="#" class="navbar-brand">Title</a>
+
+				<a href="#" class="badge badge-primary">Link1</a>
+
+				<a href="#" class="badge badge-secondary">Link2</a>
+
+				<a href="#" class="badge badge-success">Link3</a>
+
+				<a href="#" class="badge badge-danger">Link4</a>
 
 			</div>
 
@@ -39,78 +46,120 @@
 
   			<div class="jumbotron">
 
-  				<h1>HELLO WORLD!</h1>
 
-  				<p>this is home hafe gare hhild smark thass trdsdd hskaj htr fsccd kleri</p>
+  				<h1>"HELLO WORLD!"</h1>
+
+						<hr class="mt-2">
+
+  								<p>this is home hafe gare hhild smark thass trdsdd hskaj htr fsccd kleri</p>
+
+								<p class="lead">
 
   				<button class="btn btn primary" role="button">Learn More</button>
 
+				</p>
+
 			</div>
+	      <div class="media">
+
+	 				 <img class="align-self-center m-2 p-2" src="kittyplaceholder.jpg" alt="kitty placeholder">
+
+	  							<div class="media-body">
+
+	    							<h5 class="mt-1">Center-aligned media</h5>
+
+	    					<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+
+	    	<p>Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu s et magnis dis parturient montes, nascetur ridiculus mus.</p>
+
+	  </div>
+
+	</div>
 
 
 
-		<div class="container">
+	<div class="container">
 
-			<p>Lorizzle ipsum dolizzle gangsta amet, shiznit adipiscing tellivizzle. Nullizzle yippiyo velizzle, things crackalackin, dang the bizzle, gravida vizzle, arcu. Pellentesque stuff tortizzle. Sizzle eros. Fusce izzle shizzle my nizzle crocodizzle dapibizzle turpis tempizzle dawg. Mauris mah nizzle nibh et cool. Vestibulum izzle tortizzle. Pellentesque crazy rhoncizzle ma nizzle. In you son of a bizzle habitasse platea dictumst. Donec check out this. Curabitur tellus its fo rizzle, pretium eu, mattizzle izzle, eleifend vitae, yo. Daahng dawg fizzle. Yo semper velizzle sizzle purizzle.</p>
-
-			<img src="bootstrap-challenge.html" width="200" height="100">
-
-		</div>
-
-
-
-
-
-
-		<div class="container">
-
-			<div class="row">
+				<div class="row">
 
 					<div class="card border-0 col-lg-3 col-md-6 col-sm-6 col-xs-12">
 
-				<div class="card-block border rounded m-2 p-2">
 
-					<p class="card-text">Gangster interdum volutpat tellizzle. Ut for sure adipiscing lorem. Donec</p>
+						<img class="card-img-top rounded-circle w-50 m-2 p-2" src="kittyplaceholder.jpg" alt="kitty">
+
+								<div class="card-block bg-light border rounded m-1 p-1 w-50 h-50">
+
+							<p class="card-text text-center">Lorizzle ipsum dolizzle gangsta amet, shiznit  Nullizzle yippiyo velizzle,skhllllkkl</p>
+
+						</div>
 
 				</div>
 
+			</div>
+
+	</div>
+
+
+
+
+			<div class="card border-0 col-lg-3 col-md-6 col-sm-6 col-xs-12">
+
+								<img class="card-img-top rounded-circle w-50 m-2 p-2" src="kittyplaceholder.jpg" alt="kitty">
+
+									<div class="card-block bg-light border rounded m-1 p-1 w-50 h-50">
+
+								<p class="card-text text-center">Lorizzleivizzle. Nullizzle yippiyo velizzle,kalackin, dang the bizzle, gravida vizzle,</p>
+
+						</div>
+
 					</div>
 
-			 <div class="card border-0 col-lg-3 col-md-6 col-sm-6 col-xs-12">
 
-          				<div class="card-block border rounded m-2 p-2">
+			<div class="card border-0 col-lg-3 col-md-6 col-sm-6 col-xs-12">
 
-          					<p class="card-text">Gangster interdum volutpat tellizzle. Ut for sure adipiscing lorem. Donec</p>
+								<img class="card-img-top rounded-circle m-2 w-50 p-2" src="kittyplaceholder.jpg" alt="kitty">
 
-          				</div>
 
-          					</div>
+									<div class="card-block bg-light border rounded m-1 p-1 w-50 h-50">
 
-          		<div class="card border-0 col-lg-3 col-md-6 col-sm-6 col-xs-12">
+										<p class="card-text text-center">Lorizzle ipsum yo velizzle, things crackalackin, dang the bizzle tortizzle.  </p>
 
-               				<div class="card-block border rounded m-2 p-2">
+							</div>
 
-               					<p class="card-text">Gangster interdum volutpat tellizzle. Ut for sure adipiscing lorem. Donec</p>
-
-               				</div>
-
-               					</div>
-
-               <div class="card border-0 col-lg-3 col-md-6 col-sm-6 col-xs-12">
-
-               				<div class="card-block border rounded m-2 p-2">
-
-               					<p class="card-text">Gangster interdum volutpat tellizzle. Ut for sure adipiscing lorem. Donec</p>
-
-               				</div>
-
-               					</div>
+					</div>
 
 
 
 
-</div>
-</div>
+					<div class="card border-0 col-lg-3 col-md-6 col-sm-6 col-xs-12">
+
+								<img class="card-img-top rounded-circle m-2 w-50 p-2" src="kittyplaceholder.jpg" alt="kitty">
+
+
+									<div class="card-block bg-light border rounded m-1 p-1 w-50 h-50">
+
+											<p class="card-text text-center">Lorizzle ipsum velizzle, things cr crocodizzle dapibizzle turpis tempizzle dawg. </p>
+								</div>
+						</div>
 
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
